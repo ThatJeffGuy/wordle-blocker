e5c221884beedf46b463bd9f7609b5838055578c
Jeff Wilton <49243483+ThatJeffGuy@users.noreply.github.com>
2025-08-24 10:04:05 -0400

    Update wordle_blocker.plugin.js
    added better logging to confirm it's filtering correctly

--- wordle_blocker.plugin.js
+++ wordle_blocker.plugin.js
@@ -2,7 +2,7 @@
  * @name WordleBlocker
  * @author ScottishHaze
  * @description Blocks Wordle celebrations and similar puzzle game results from chat
- * @version 2.5-beta
+ * @version 2.6-beta
  * @donate https://www.paypal.com/donate?token=TDoql1alt1c365GK9gdbysf0hHFKmbjjHgW93Kn_al8__EduYfvG41Peg_H_TNpI64JiGHs5l5Nvpu2w
  * @patreon None -- PayPal link above.
  * @website https://www.everydaysciencestuff.com/
